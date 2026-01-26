cc9d9e7d0dfe4ee1011336d26d72a42c9b0af7b3
Symniya <symniya09@gmail.com>
2026-01-26 17:00:26 +0530

    data-ingestion updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,7 +27,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         
         try:
-            df = pd.read_csv("notebook/data/stud.csv")  #reading the data from the source
+            df = pd.read_csv("notebook/data/stud.csv")  #THIS IS THE SOURCE,CAN CHANGE ONLY THIS ANYTIME A NEW DATA SOURCE(API,DB CONNECTS,CSV ETC) COMES IN, THE REST OF THE CODE REMAINS SAME
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True) #creating the artifact folder if not exists
